5aa80e5f13e3d6ea6a3978d31832ab80b9905900
Dirk Niggemann <dirk.niggemann@gmail.com>
2020-11-04 19:28:57 +0000

    Added working Teensy examples

--- examples/SpeedTest/SpeedTest.ino
+++ examples/SpeedTest/SpeedTest.ino
@@ -1,14 +1,22 @@
 // Simple speed test for filesystem objects
 // Released to the public domain by Earle F. Philhower, III
 
+
+#include <FastCRC.h>
+
+FastCRC32 CRC32;
+
 #include <LittleFS.h>
 
-SPIFlash FLASH(23, 0x0160);
-FS LittleFS = FS(FSImplPtr(new littlefs_impl::LittleFSImpl(&FLASH, FS_PHYS_ADDR, 0, /*FS_PHYS_SIZE,*/ FS_PHYS_PAGE, FS_PHYS_BLOCK, FS_MAX_OPEN_FILES)));
-
 #define TESTSIZEKB 512
+#define TESTBUF 512
+#define NUMBUFSKB 2
+#define FLASH_SS 6
+SPIFlash * FLASH = NULL;
+FS * LittleFS = NULL;
 
 void DoTest(FS *fs) {
+  Serial.println("Formatting");
   if (!fs->format()) {
     Serial.printf("Unable to format(), aborting\n");
     return;
@@ -18,8 +26,8 @@ void DoTest(FS *fs) {
     return;
   }
 
-  uint8_t data[256];
-  for (int i = 0; i < 256; i++) {
+  uint8_t data[TESTBUF +1];
+  for (int i = 0; i < TESTBUF; i++) {
     data[i] = (uint8_t) i;
   }
 
@@ -31,62 +39,109 @@ void DoTest(FS *fs) {
     return;
   }
   for (int i = 0; i < TESTSIZEKB; i++) {
-    for (int j = 0; j < 4; j++) {
-      f.write(data, 256);
+    for (int j = 0; j < NUMBUFSKB; j++) {
+      uint16_t sect = i*NUMBUFSKB + j;
+      data[1] = sect >> 8;
+      data[2] = sect & 0xff;
+      uint32_t crc = CRC32.crc32(data+1, TESTBUF -5);
+      memcpy(data + (TESTBUF -4), &crc, 4);
+      f.write(data, TESTBUF);
+      //Serial.printf("Sect %d CRC %lx\n", sect, crc);
     }
   }
   f.close();
   long stop = millis();
-  Serial.printf("==> Time to write %dKB in 256b chunks = %ld milliseconds\n", TESTSIZEKB, stop - start);
+  Serial.printf("==> Time to write %dKB in %db chunks = %ld milliseconds\n", TESTSIZEKB, TESTBUF, stop - start);
 
   f = fs->open("/testwrite.bin", "r");
   Serial.printf("==> Created file size = %d\n", f.size());
   f.close();
 
-  Serial.printf("Reading %dKB file sequentially in 256b chunks\n", TESTSIZEKB);
+  Serial.printf("Reading %dKB file sequentially in %db chunks\n", TESTSIZEKB, TESTBUF);
   start = millis();
   f = fs->open("/testwrite.bin", "r");
   for (int i = 0; i < TESTSIZEKB; i++) {
-    for (int j = 0; j < 4; j++) {
-      f.read(data, 256);
+    for (int j = 0; j < NUMBUFSKB; j++) {
+       memset(data, 0, TESTBUF+1);
+      f.read(data, TESTBUF);
+      uint16_t sect = i*NUMBUFSKB + j;
+      uint16_t gotsect = (data[1]<<8) | (data[2] & 0xff);
+      uint32_t gotcrc;
+      uint32_t crc =  CRC32.crc32(data+1, TESTBUF -5);
+      memcpy(&gotcrc, data + (TESTBUF -4), 4);
+      
+      if (sect != gotsect) {
+        Serial.printf ("Error sect: %d %d\n", sect, gotsect);
+      }
+      if(gotcrc != crc) {
+         Serial.printf ("CRC Error sect: %lx %lx\n", crc, gotcrc);
+      }
     }
   }
   f.close();
   stop = millis();
-  Serial.printf("==> Time to read %dKB sequentially in 256b chunks = %ld milliseconds = %ld bytes/s\n", TESTSIZEKB, stop - start, TESTSIZEKB * 1024 / (stop - start) * 1000);
+  Serial.printf("==> Time to read %dKB sequentially in %db chunks = %ld milliseconds = %ld bytes/s\n", TESTSIZEKB, TESTBUF, stop - start, TESTSIZEKB * 1024 / (stop - start) * 1000);
 
-  Serial.printf("Reading %dKB file MISALIGNED in flash and RAM sequentially in 256b chunks\n", TESTSIZEKB);
+  Serial.printf("Reading %dKB file MISALIGNED in flash and RAM sequentially in %db chunks\n", TESTSIZEKB, TESTBUF);
   start = millis();
   f = fs->open("/testwrite.bin", "r");
   f.read();
   for (int i = 0; i < TESTSIZEKB; i++) {
-    for (int j = 0; j < 4; j++) {
-      f.read(data + 1, 256);
+    for (int j = 0; j < NUMBUFSKB; j++) {
+      memset(data, 0, TESTBUF+1);
+      f.read(data + 1, TESTBUF);
+      uint16_t sect = i*NUMBUFSKB + j;
+      uint16_t gotsect = (data[1]<<8) | (data[2] & 0xff);
+      uint32_t gotcrc = 0;
+      uint32_t crc =  CRC32.crc32(data+1, TESTBUF -5);
+      memcpy(&gotcrc, data + (TESTBUF -4), 4);
+      
+      if (sect != gotsect) {
+        Serial.printf ("Error sect: %d %d\n", sect, gotsect);
+      }
+      if(gotcrc != crc) {
+         Serial.printf ("CRC Error sect: %lx %lx\n", crc, gotcrc);
+      }
     }
   }
   f.close();
   stop = millis();
-  Serial.printf("==> Time to read %dKB sequentially MISALIGNED in flash and RAM in 256b chunks = %ld milliseconds = %ld bytes/s\n", TESTSIZEKB, stop - start, TESTSIZEKB * 1024 / (stop - start) * 1000);
+  Serial.printf("==> Time to read %dKB sequentially MISALIGNED in flash and RAM in %db chunks = %ld milliseconds = %ld bytes/s\n", TESTSIZEKB, TESTBUF, stop - start, TESTSIZEKB * 1024 / (stop - start) * 1000);
 
 
-  Serial.printf("Reading %dKB file in reverse by 256b chunks\n", TESTSIZEKB);
+  Serial.printf("Reading %dKB file in reverse by %db chunks\n", TESTSIZEKB, TESTBUF);
   start = millis();
   f = fs->open("/testwrite.bin", "r");
   for (int i = 0; i < TESTSIZEKB; i++) {
-    for (int j = 0; j < 4; j++) {
-      if (!f.seek(256 + 256 * j * i, SeekEnd)) {
-        Serial.printf("Unable to seek to %d, aborting\n", -256 - 256 * j * i);
+    for (int j = 0; j < NUMBUFSKB; j++) {
+      uint32_t pos = TESTBUF + (TESTBUF * i*NUMBUFSKB)  + (j*TESTBUF);
+      //Serial.printf("pos %ld\n", pos);
+      if (!f.seek(pos, SeekEnd)) {
+        Serial.printf("Unable to seek to %d, aborting\n", -TESTBUF - (TESTBUF * j * i));
         return;
       }
-      if (256 != f.read(data, 256)) {
-        Serial.printf("Unable to read 256 bytes, aborting\n");
+      memset(data, 0, TESTBUF+1);
+      if (TESTBUF != f.read(data, TESTBUF)) {
+        Serial.printf("Unable to read %d bytes, aborting\n", TESTBUF);
         return;
+      }
+      uint16_t sect = (TESTSIZEKB * NUMBUFSKB -1) - (i*NUMBUFSKB + j);
+      uint16_t gotsect = (data[1]<<8) | (data[2] & 0xff);
+      uint32_t gotcrc = 0;
+      uint32_t crc =  CRC32.crc32(data+1, TESTBUF -5);
+      memcpy(&gotcrc, data + (TESTBUF -4), 4);
+      
+      if (sect != gotsect) {
+        Serial.printf ("Error sect: %d %d\n", sect, gotsect);
+      }
+      if(gotcrc != crc) {
+         Serial.printf ("CRC Error sect: %lx %lx\n", crc, gotcrc);
       }
     }
   }
   f.close();
   stop = millis();
-  Serial.printf("==> Time to read %dKB in reverse in 256b chunks = %ld milliseconds = %ld bytes/s\n", TESTSIZEKB, stop - start, TESTSIZEKB * 1024 / (stop - start) * 1000);
+  Serial.printf("==> Time to read %dKB in reverse in %db chunks = %ld milliseconds = %ld bytes/s\n", TESTSIZEKB, TESTBUF, stop - start, TESTSIZEKB * 1024 / (stop - start) * 1000);
 
 
   Serial.printf("Writing 64K file in 1-byte chunks\n");
@@ -114,15 +169,20 @@ void DoTest(FS *fs) {
 }
 
 void setup() {
+
   Serial.begin(115200);
+  Serial.printf("Set Pin Mode\n");
+  
+  FLASH = new SPIFlash(FLASH_SS);
+  FLASH->begin();
+  delay(1000);
+  LittleFS = new FS(FSImplPtr(new littlefs_impl::LittleFSImpl(FLASH, FS_PHYS_ADDR, 0, /*FS_PHYS_SIZE,*/ FS_PHYS_PAGE, FS_PHYS_BLOCK, FS_MAX_OPEN_FILES)));
   Serial.printf("Beginning LittleFS test\n");
   Serial.flush();
-  DoTest(&LittleFS);
-  //Serial.printf("Beginning SPIFFS test\n");
-  //Serial.flush();
-  //DoTest(&SPIFFS);
+  DoTest(LittleFS);
 }
 
 void loop() {
+  Serial.println("Done!");
   delay(10000);
 }
--- examples/listfiles/listfiles.ino
+++ examples/listfiles/listfiles.ino
@@ -1,80 +1,104 @@
 /*
-  Listfiles
-
-  This example shows how print out the files in a
-  directory on a SD card
-
-  The circuit:
-   SD card attached to SPI bus as follows:
- ** MOSI - pin 11
+  SD card basic file example
+ 
+ This example shows how to create and destroy an SD card file 	
+ The circuit:
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11, pin 7 on Teensy with audio board
  ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4
-
-  created   Nov 2010
-  by David A. Mellis
-  modified 9 Apr 2012
-  by Tom Igoe
-  modified 2 Feb 2014
-  by Scott Fitzgerald
-
-  This example code is in the public domain.
-
-*/
+ ** CLK - pin 13, pin 14 on Teensy with audio board
+ ** CS - pin 4, pin 10 on Teensy with audio board
+ 
+ created   Nov 2010
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+ 
+ This example code is in the public domain.
+ 	 
+ */
+#include <LittleFS.h>
 #include <SPI.h>
-#include <SD.h>
+#include <SPIMemory.h>
+
+
+
+SPIFlash * FLASH = NULL;
+FS * LittleFS = NULL;
 
 File root;
 
-void setup() {
+// change this to match your SD shield or module;
+// Arduino Ethernet shield: pin 4
+// Adafruit SD shields and modules: pin 10
+// Sparkfun SD shield: pin 8
+// Teensy audio board: pin 10
+// Teensy 3.5 & 3.6 on-board: BUILTIN_SDCARD
+// Wiz820+SD board: pin 4
+// Teensy 2.0: pin 0
+// Teensy++ 2.0: pin 20
+#define FLASH_SS 6
+
+void setup()
+{;
+
+  //UNCOMMENT THESE TWO LINES FOR TEENSY AUDIO BOARD:
+  //SPI.setMOSI(7);  // Audio shield has MOSI on pin 7
+  //SPI.setSCK(14);  // Audio shield has SCK on pin 14  
+  //SPI.begin();
+  //SPI.setFrequency(500000);
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
 
-  Serial.print("Initializing SD card...");
 
-  if (!SD.begin(SS)) {
+  Serial.print("Initializing LittleFS...");
+  FLASH = new SPIFlash(FLASH_SS);
+  FLASH->begin();
+  Serial.printf("ID: %x\n",FLASH->getJEDECID());
+  LittleFS = new FS(FSImplPtr(new littlefs_impl::LittleFSImpl(FLASH, FS_PHYS_ADDR, 0, /*FS_PHYS_SIZE,*/ FS_PHYS_PAGE, FS_PHYS_BLOCK, FS_MAX_OPEN_FILES)));
+  if (!LittleFS->begin()) {
     Serial.println("initialization failed!");
     return;
   }
+  //SPI.setClockDivider(SPI_CLOCK_DIV1);
   Serial.println("initialization done.");
 
-  root = SD.open("/");
-
+  root = LittleFS->open("/", FILE_READ);
+  
   printDirectory(root, 0);
-
+  
   Serial.println("done!");
 }
 
-void loop() {
+void loop()
+{
   // nothing happens after setup finishes.
 }
 
 void printDirectory(File dir, int numTabs) {
-  while (true) {
-
-    File entry =  dir.openNextFile();
-    if (! entry) {
-      // no more files
-      break;
-    }
-    for (uint8_t i = 0; i < numTabs; i++) {
-      Serial.print('\t');
-    }
-    Serial.print(entry.name());
-    if (entry.isDirectory()) {
-      Serial.println("/");
-      printDirectory(entry, numTabs + 1);
-    } else {
-      // files have sizes, directories do not
-      Serial.print("\t\t");
-      Serial.print(entry.size(), DEC);
-      time_t cr = entry.getCreationTime();
-      time_t lw = entry.getLastWrite();
-      struct tm * tmstruct = localtime(&cr);
-      Serial.printf("\tCREATION: %d-%02d-%02d %02d:%02d:%02d", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
-      tmstruct = localtime(&lw);
-      Serial.printf("\tLAST WRITE: %d-%02d-%02d %02d:%02d:%02d\n", (tmstruct->tm_year) + 1900, (tmstruct->tm_mon) + 1, tmstruct->tm_mday, tmstruct->tm_hour, tmstruct->tm_min, tmstruct->tm_sec);
-    }
-    entry.close();
-  }
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       Serial.println("**nomorefiles**");
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
 }
